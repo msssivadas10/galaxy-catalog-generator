4ee83fa50be4102d51d21c9fa149196619e1477e
ms3 <ms3.msssivadas@gmail.com>
2025-08-24 19:10:53 +0530

    added setup.py

---
+++ src/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, Extension
+from setuptools.command.build_ext import build_ext
+import subprocess
+
+class BuildShared(build_ext):
+    def run(self):
+        # compile Fortran to shared lib manually
+        subprocess.check_call(
+            ["gfortran", "-shared", "-fPIC", "-J", ".include", 
+             "quadutils.f90", "powerspectrum.f90", "power_integrals.f90", 
+             "-o", "libpowerspectrum.so"]
+        )
+
+setup(
+    name="mymod",
+    version="0.1",
+    cmdclass={"build_ext": BuildShared},
+)
