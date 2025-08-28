aa809f48b0ff614cf6b9bbff497935987e4ac402
ms3 <ms3.msssivadas@gmail.com>
2025-08-28 19:23:33 +0530

    update

--- setup.py
+++ setup.py
@@ -7,13 +7,12 @@ class BuildShared(build_ext):
         # compile Fortran to shared lib manually
         files = [
             os.path.join("src", "utils", "integrate.f90"),
-            os.path.join("src", "power", "growthfactor.f90"),
-            os.path.join("src", "power", "correlation.f90"),
-            os.path.join("src", "power", "variance.f90"),
-            os.path.join("src", "power", "models", "ingredients.f90"),
-            os.path.join("src", "power", "models", "eisenstein98_zb.f90"),
-            os.path.join("src", "power", "models", "eisenstein98_mnu.f90"),
-            os.path.join("src", "power", "models", "eisenstein98_bao.f90"),
+            os.path.join("src", "growthfactor.f90"),
+            os.path.join("src", "correlation.f90" ),
+            os.path.join("src", "variance.f90"    ),
+            os.path.join("src", "models", "power_spectrum.f90"),
+            os.path.join("src", "models", "mass_function.f90" ),
+            os.path.join("src", "models", "halo_bias.f90"     ),
         ]
         args = [ "-shared", "-fPIC", "-J", ".include" ]
         subprocess.check_call(
--- x.py
+++ x.py
@@ -1,203 +1,270 @@
 
-# import numpy as np
-# import matplotlib.pyplot as plt
-# import numpy.ctypeslib as npct
-# import ctypes
+import numpy as np
+import matplotlib.pyplot as plt
+import numpy.ctypeslib as npct
+import ctypes
 
-# # load the shared library
-# lib = npct.load_library("libpowerspectrum", ".")
+# load the shared library
+lib = npct.load_library("libpowerspectrum", ".")
 
-# class lgargs_t(ctypes.Structure):
-#     _fields_ = [
-#         ('Om0'    , ctypes.c_double), 
-#         ('Ode0'   , ctypes.c_double),
-#         ('w0'     , ctypes.c_double),
-#         ('wa'     , ctypes.c_double),
-#         ('abstol' , ctypes.c_double),
-#         ('reltol' , ctypes.c_double),
-#         ('maxiter', ctypes.c_int64 ),
-#     ]
+class lgargs_t(ctypes.Structure):
+    _fields_ = [
+        ('Om0'    , ctypes.c_double), 
+        ('Ode0'   , ctypes.c_double),
+        ('w0'     , ctypes.c_double),
+        ('wa'     , ctypes.c_double),
+        ('abstol' , ctypes.c_double),
+        ('reltol' , ctypes.c_double),
+        ('maxiter', ctypes.c_int64 ),
+    ]
 
-# lib.linear_growth.argtypes = [
-#     ctypes.c_double, 
-#     ctypes.c_int, 
-#     ctypes.POINTER(lgargs_t), 
-# ]
-# lib.linear_growth.restype = ctypes.c_double
-# linear_growth = np.vectorize(lib.linear_growth, excluded=[2])
+lib.linear_growth.argtypes = [
+    ctypes.c_double, 
+    ctypes.c_int, 
+    ctypes.POINTER(lgargs_t), 
+]
+lib.linear_growth.restype = ctypes.c_double
+linear_growth = np.vectorize(lib.linear_growth, excluded=[2])
 
-# args_gf = lgargs_t()
-# args_gf.Om0     =  0.3
-# args_gf.Ode0    =  0.7
-# args_gf.w0      = -1.
-# args_gf.wa      =  0.
-# args_gf.abstol  =  1e-08
-# args_gf.reltol  =  1e-08
-# args_gf.maxiter =  50
+class psargs_t(ctypes.Structure):
+    _fields_ = [
+        ('z'         , ctypes.c_double  ),
+        ('h'         , ctypes.c_double  ), 
+        ('Omh2'      , ctypes.c_double  ),
+        ('Obh2'      , ctypes.c_double  ),
+        ('Onuh2'     , ctypes.c_double  ),
+        ('Nnu'       , ctypes.c_double  ),        
+        ('ns'        , ctypes.c_double  ),
+        ('sigma8'    , ctypes.c_double  ),
+        ('theta'     , ctypes.c_double  ),
+        ('dplus_z'   , ctypes.c_double  ),        
+        ('dplus_0'   , ctypes.c_double  ),        
+        ('norm'      , ctypes.c_double  ),        
+        ('include_nu', ctypes.c_int     ),
+        ('z_eq'      , ctypes.c_double  ),
+        ('z_d'       , ctypes.c_double  ),        
+        ('s'         , ctypes.c_double  ),
+        ('k_silk'    , ctypes.c_double  ),
+        ('param'     , ctypes.c_double*5),
+    ]
 
-# class psargs_t(ctypes.Structure):
-#     _fields_ = [
-#         ('z'         , ctypes.c_double  ),
-#         ('h'         , ctypes.c_double  ), 
-#         ('Omh2'      , ctypes.c_double  ),
-#         ('Obh2'      , ctypes.c_double  ),
-#         ('Onuh2'     , ctypes.c_double  ),
-#         ('Nnu'       , ctypes.c_double  ),        
-#         ('ns'        , ctypes.c_double  ),
-#         ('sigma8'    , ctypes.c_double  ),
-#         ('theta'     , ctypes.c_double  ),
-#         ('dplus_z'   , ctypes.c_double  ),        
-#         ('dplus_0'   , ctypes.c_double  ),        
-#         ('norm'      , ctypes.c_double  ),        
-#         ('include_nu', ctypes.c_int     ),
-#         ('z_eq'      , ctypes.c_double  ),
-#         ('z_d'       , ctypes.c_double  ),        
-#         ('s'         , ctypes.c_double  ),
-#         ('k_silk'    , ctypes.c_double  ),
-#         ('param'     , ctypes.c_double*5),
-#     ]
+CALLBACK = ctypes.CFUNCTYPE(
+    ctypes.c_double,    # return type
+    ctypes.c_double,    # x input
+    ctypes.c_void_p     # ctx pointer
+)
 
-# CALLBACK = ctypes.CFUNCTYPE(
-#     ctypes.c_double,    # return type
-#     ctypes.c_double,    # x input
-#     ctypes.c_void_p     # ctx pointer
-# )
+def as_callback(fn):
+    def wrapper(lnk, argsp):
+        args_ps = ctypes.cast(argsp, ctypes.POINTER(psargs_t)).contents 
+        return fn(lnk, ctypes.byref(args_ps))
+    return CALLBACK(wrapper)
 
-# def as_callback(fn):
-#     def wrapper(lnk, argsp):
-#         args = ctypes.cast(argsp, ctypes.POINTER(psargs_t)).contents 
-#         return fn(lnk, ctypes.byref(args))
-#     return CALLBACK(wrapper)
+# define argument and return types
+lib.init_eisenstein98_zb.argtypes = [ ctypes.POINTER(psargs_t) ]
+lib.init_eisenstein98_zb.restype  = None
 
-# # define argument and return types
-# lib.init_eisenstein98_zb.argtypes = [ ctypes.POINTER(psargs_t) ]
-# lib.init_eisenstein98_zb.restype  = None
+lib.ps_eisenstein98_zb.argtypes = [ ctypes.c_double, ctypes.POINTER(psargs_t) ]
+lib.ps_eisenstein98_zb.restype  = ctypes.c_double
+ps_eisenstein98_zb = np.vectorize(lib.ps_eisenstein98_zb)
 
-# lib.ps_eisenstein98_zb.argtypes = [ ctypes.c_double, ctypes.POINTER(psargs_t) ]
-# lib.ps_eisenstein98_zb.restype  = ctypes.c_double
-# ps_eisenstein98_zb = np.vectorize(lib.ps_eisenstein98_zb)
+lib.variance.argtypes = [
+    ctypes.c_double, 
+    ctypes.c_int, 
+    ctypes.c_int, 
+    ctypes.c_int, 
+    np.ctypeslib.ndpointer(dtype=np.float64, ndim=2, flags='C_CONTIGUOUS'), 
+    ctypes.c_int64,
+    ctypes.c_int,
+]
+lib.variance.restype = ctypes.c_double
+variance = np.vectorize(lib.variance, excluded=[4])
 
-# args = psargs_t()
-# args.z       = 0. 
-# args.h       = 0.7 
-# args.Omh2    = args_gf.Om0 * args.h**2    
-# args.Obh2    = 0.04        * args.h**2    
-# args.ns      = 0.9  
-# args.sigma8  = 0.8     
-# args.theta   = 2.725 / 2.7    
-# args.norm    = 1.    
-# args.dplus_z = linear_growth(args.z, 0, ctypes.byref(args_gf))       
-# args.dplus_0 = linear_growth(0.    , 0, ctypes.byref(args_gf))       
+lib.correlation.argtypes = [
+    ctypes.c_double, 
+    ctypes.c_int, 
+    np.ctypeslib.ndpointer(dtype=np.float64, ndim=2, flags='C_CONTIGUOUS'), 
+    ctypes.c_int64,
+    ctypes.c_int,
+]
+lib.correlation.restype = ctypes.c_double
+correlation = np.vectorize(lib.correlation, excluded=[2])
 
-# lib.init_eisenstein98_zb( ctypes.byref(args) )
-# # print('z          = ', args.z         )
-# # print('h          = ', args.h         )
-# # print('Omh2       = ', args.Omh2      )
-# # print('Obh2       = ', args.Obh2      )
-# # print('Onuh2      = ', args.Onuh2     )
-# # print('Nnu        = ', args.Nnu       )
-# # print('ns         = ', args.ns        )
-# # print('sigma8     = ', args.sigma8    )
-# # print('theta      = ', args.theta     )
-# # print('dplus_z    = ', args.dplus_z   )
-# # print('dplus_0    = ', args.dplus_0   )
-# # print('norm       = ', args.norm      )
-# # print('include_nu = ', args.include_nu)
-# # print('z_eq       = ', args.z_eq      )
-# # print('z_d        = ', args.z_d       )
-# # print('s          = ', args.s         )
-# # print('k_silk     = ', args.k_silk    )
-# # print('param      = ', list(args.param))
+class hmfargs_t(ctypes.Structure):
+    _fields_ = [
+        ('z'       , ctypes.c_double   ),
+        ('lnm'     , ctypes.c_double   ),
+        ('H0'      , ctypes.c_double   ),        
+        ('Om0'     , ctypes.c_double   ),
+        ('Delta_m' , ctypes.c_double   ),
+        ('s'       , ctypes.c_double   ),
+        ('dlnsdlnm', ctypes.c_double   ), 
+        ('rho_m'   , ctypes.c_double   ),
+        ('param'   , ctypes.c_double*16),        
+    ]
 
-# lib.variance.argtypes = [
-#     ctypes.c_double, 
-#     ctypes.c_int, 
-#     ctypes.c_int, 
-#     ctypes.c_int, 
-#     np.ctypeslib.ndpointer(dtype=np.float64, ndim=2, flags='C_CONTIGUOUS'), 
-#     ctypes.c_int64,
-#     ctypes.c_int,
-# ]
-# lib.variance.restype = ctypes.c_double
-# variance = np.vectorize(lib.variance, excluded=[4])
+lib.setup_hmf.argtypes = [
+    ctypes.POINTER(hmfargs_t), 
+    ctypes.c_int, 
+    np.ctypeslib.ndpointer(dtype=np.float64, ndim=2, flags='C_CONTIGUOUS'), 
+    ctypes.c_int64,
+    ctypes.c_int,
+]
+lib.setup_hmf.restype = None
 
-# lib.correlation.argtypes = [
-#     ctypes.c_double, 
-#     ctypes.c_int, 
-#     np.ctypeslib.ndpointer(dtype=np.float64, ndim=2, flags='C_CONTIGUOUS'), 
-#     ctypes.c_int64,
-#     ctypes.c_int,
-# ]
-# lib.correlation.restype = ctypes.c_double
-# correlation = np.vectorize(lib.correlation, excluded=[2])
+lib.setup_hmf_tinker08.argtypes = [
+    ctypes.POINTER(hmfargs_t), 
+    ctypes.c_int, 
+    np.ctypeslib.ndpointer(dtype=np.float64, ndim=2, flags='C_CONTIGUOUS'), 
+    ctypes.c_int64,
+    ctypes.c_int,
+]
+lib.setup_hmf_tinker08.restype = None
 
-# pktab = np.zeros((1001, 2))
-# pktab[:, 0] = np.linspace( np.log(1e-04), np.log(1e+04), pktab.shape[0] )
-# pktab[:, 1] = ps_eisenstein98_zb(pktab[:, 0], ctypes.byref(args))
+lib.hmf_press74.argtypes = [ ctypes.POINTER(hmfargs_t), ctypes.c_int ]
+lib.hmf_press74.restype  = ctypes.c_double
 
-# var8_calc  = variance( np.log(8./args.h), 0, 0, 0, pktab, pktab.shape[0], pktab.shape[1] )
-# args.norm  = args.sigma8**2 / var8_calc 
-# pktab[:,1] = pktab[:,1] + np.log(args.norm)
-# # print( variance( np.log(8./args.h), 0, 0, 0, pktab, pktab.shape[0], pktab.shape[1] ), args.sigma8**2 )
+lib.hmf_sheth01.argtypes = [ ctypes.POINTER(hmfargs_t), ctypes.c_int ]
+lib.hmf_sheth01.restype  = ctypes.c_double
 
-# # z = np.linspace(0., 10, 11)
-# # y = linear_growth(z, 0, ctypes.byref(args_gf))
-# # plt.plot(z, y, 'o-')
-# # plt.show()
+lib.hmf_jenkins01.argtypes = [ ctypes.POINTER(hmfargs_t), ctypes.c_int ]
+lib.hmf_jenkins01.restype  = ctypes.c_double
 
-# # k = np.logspace(-4, 3, 11)
-# # y = np.exp( ps_eisenstein98_zb(np.log(k), ctypes.byref(args)) )
-# # plt.loglog(k, y, '-o')
-# # plt.show()
+lib.hmf_reed03.argtypes = [ ctypes.POINTER(hmfargs_t), ctypes.c_int ]
+lib.hmf_reed03.restype  = ctypes.c_double
 
-# # r = np.logspace(-3, 2, 11)
-# # s = variance( np.log(r), 0, 0, 0, pktab, pktab.shape[0], pktab.shape[1] )
-# # c = correlation( np.log(r), 0, pktab, pktab.shape[0], pktab.shape[1] )
-# # plt.loglog(r, s, '-o')
-# # plt.loglog(r, c, '-o')
-# # plt.show()
+lib.hmf_tinker08.argtypes = [ ctypes.POINTER(hmfargs_t), ctypes.c_int ]
+lib.hmf_tinker08.restype  = ctypes.c_double
 
+lib.hmf_courtin10.argtypes = [ ctypes.POINTER(hmfargs_t), ctypes.c_int ]
+lib.hmf_courtin10.restype  = ctypes.c_double
 
-x  = [ None, 200., 300., 400., 600., 800.,1200.,1600.,2400.,3200.,]
-y  = [None, 0.186, 0.200, 0.212, 0.218, 0.248, 0.255, 0.260, 0.260, 0.260, ]
-y2 = [ None ] * len(x)
-u  = [ None ] * (len(x) - 1)
+lib.hmf_crocce10.argtypes = [ ctypes.POINTER(hmfargs_t), ctypes.c_int ]
+lib.hmf_crocce10.restype  = ctypes.c_double
 
-n = len(x)-1
-y2[1] = 0.
-u[1]  = 0.
-for i in range(2, n):
-    sig = ( x[i] - x[i-1] ) / ( x[i+1] - x[i-1] )
-    p   = sig * y2[i-1] + 2
-    y2[i] = ( sig - 1 ) / p
-    u[i]  = ( 
-        6*(
-            ( y[i+1] - y[i] ) / ( x[i+1] - x[i] ) - ( y[i] - y[i-1] ) / ( x[i] - x[i-1] )
-        ) / ( x[i+1] - x[i-1] ) - sig * u[i-1] 
-    ) / p
-y2[n] = 0
-for k in range(n-1, 0, -1):
-    y2[k] = y2[k] * y2[k+1] + u[k]
+lib.hbf_cole89.argtypes = [ ctypes.POINTER(hmfargs_t) ] 
+lib.hbf_cole89.restype  = ctypes.c_double
 
+lib.hbf_sheth01.argtypes = [ ctypes.POINTER(hmfargs_t) ] 
+lib.hbf_sheth01.restype  = ctypes.c_double
 
-import numpy as np, matplotlib.pyplot as plt
-xs = np.linspace(200, 3200, 201)
-ys = []
-for xi in xs:
-    klo = 1
-    khi = n
-    while (khi-klo > 1):
-        k = (khi+klo) // 2
-        if ( x[k] > xi ):
-            khi = k
-        else:
-            klo = k
-    h  = x[khi] - x[klo]
-    a  = ( x[khi] - xi )/h
-    b  = ( xi - x[klo] )/h
-    yi = a*y[klo] + b*y[khi] + ((a**3 - a)*y2[klo] + (b**3 - b)*y2[khi])*(h*h)/6.
-    ys.append(yi)
+lib.hbf_tinker10.argtypes = [ ctypes.POINTER(hmfargs_t) ]
+lib.hbf_tinker10.restype  = ctypes.c_double
 
-plt.plot(x, y, 'o')
-plt.plot(xs, ys, '--')
-plt.show()
+############################################################################################################
+
+args_gf = lgargs_t()
+args_gf.Om0     =  0.3
+args_gf.Ode0    =  0.7
+args_gf.w0      = -1.
+args_gf.wa      =  0.
+args_gf.abstol  =  1e-08
+args_gf.reltol  =  1e-08
+args_gf.maxiter =  100
+
+args_ps = psargs_t()
+args_ps.z       = 0. 
+args_ps.h       = 0.7 
+args_ps.Omh2    = args_gf.Om0 * args_ps.h**2    
+args_ps.Obh2    = 0.04        * args_ps.h**2    
+args_ps.ns      = 0.9  
+args_ps.sigma8  = 0.8     
+args_ps.theta   = 2.725 / 2.7    
+args_ps.norm    = 1.    
+args_ps.dplus_z = linear_growth(args_ps.z, 0, ctypes.byref(args_gf))       
+args_ps.dplus_0 = linear_growth(0.       , 0, ctypes.byref(args_gf))       
+
+lib.init_eisenstein98_zb( ctypes.byref(args_ps) )
+# print('z          = ', args_ps.z         )
+# print('h          = ', args_ps.h         )
+# print('Omh2       = ', args_ps.Omh2      )
+# print('Obh2       = ', args_ps.Obh2      )
+# print('Onuh2      = ', args_ps.Onuh2     )
+# print('Nnu        = ', args_ps.Nnu       )
+# print('ns         = ', args_ps.ns        )
+# print('sigma8     = ', args_ps.sigma8    )
+# print('theta      = ', args_ps.theta     )
+# print('dplus_z    = ', args_ps.dplus_z   )
+# print('dplus_0    = ', args_ps.dplus_0   )
+# print('norm       = ', args_ps.norm      )
+# print('include_nu = ', args_ps.include_nu)
+# print('z_eq       = ', args_ps.z_eq      )
+# print('z_d        = ', args_ps.z_d       )
+# print('s          = ', args_ps.s         )
+# print('k_silk     = ', args_ps.k_silk    )
+# print('param      = ', list(args_ps.param))
+
+pktab = np.zeros((1001, 2))
+pktab[:, 0] = np.linspace( np.log(1e-04), np.log(1e+04), pktab.shape[0] )
+pktab[:, 1] = ps_eisenstein98_zb(pktab[:, 0], ctypes.byref(args_ps))
+
+var8_calc  = variance( np.log(8./args_ps.h), 0, 0, 0, pktab, pktab.shape[0], pktab.shape[1] )
+args_ps.norm  = args_ps.sigma8**2 / var8_calc 
+pktab[:,1] = pktab[:,1] + np.log(args_ps.norm)
+# print( variance( np.log(8./args_ps.h), 0, 0, 0, pktab, pktab.shape[0], pktab.shape[1] ), args_ps.sigma8**2 )
+
+args_mf = hmfargs_t()
+args_mf.z       = args_ps.z
+args_mf.lnm     = np.log(1e+10)
+args_mf.H0      = 70.0
+args_mf.Om0     = args_gf.Om0
+args_mf.Delta_m = 200.
+
+lib.setup_hmf_tinker08(ctypes.byref(args_mf), 0, pktab, pktab.shape[0], pktab.shape[1])
+# print('z        = ', args_mf.z          )
+# print('lnm      = ', args_mf.lnm        )
+# print('H0       = ', args_mf.H0         )
+# print('Om0      = ', args_mf.Om0        )
+# print('Delta_m  = ', args_mf.Delta_m    )
+# print('s        = ', args_mf.s          )
+# print('dlnsdlnm = ', args_mf.dlnsdlnm   )
+# print('rho_m    = ', args_mf.rho_m      )
+# print('param    = ', list(args_mf.param))
+
+# z = np.linspace(0., 10, 11)
+# y = linear_growth(z, 0, ctypes.byref(args_gf))
+# plt.plot(z, y, 'o-')
+# plt.show()
+
+# k = np.logspace(-4, 3, 11)
+# y = np.exp( ps_eisenstein98_zb(np.log(k), ctypes.byref(args_ps)) )
+# plt.loglog(k, y, '-o')
+# plt.show()
+
+# r = np.logspace(-3, 2, 11)
+# s = variance( np.log(r), 0, 0, 0, pktab, pktab.shape[0], pktab.shape[1] )
+# c = correlation( np.log(r), 0, pktab, pktab.shape[0], pktab.shape[1] )
+# plt.loglog(r, s, '-o')
+# plt.loglog(r, c, '-o')
+# plt.show()
+
+# s = np.logspace(-1, 3, 21)
+# f = np.zeros_like(s)
+# for model in ['press74', 'sheth01', 'jenkins01', 'reed03', 'tinker08', 'courtin10', 'crocce10']:
+#     func = getattr(lib, 'hmf_' + model)
+#     for i in range(s.shape[0]):
+#         args_mf.s = s[i]
+#         f[i]      = func(ctypes.byref(args_mf), -1) 
+#     plt.semilogx(s, f, 'o-', label = model)
+# plt.legend(title = 'halo massfunction)
+# plt.show()
+
+# s = np.logspace(-1, 3, 21)
+# f = np.zeros_like(s)
+# for model in ['cole89', 'sheth01', 'tinker10']:
+#     func = getattr(lib, 'hbf_' + model)
+#     for i in range(s.shape[0]):
+#         args_mf.s = s[i]
+#         f[i]      = func(ctypes.byref(args_mf)) 
+#     plt.loglog(1.686/s, f, 'o-', label = model)
+# plt.legend(title = 'halo bias')
+# plt.show()
+
+# m = np.logspace(8, 20, 11)
+# n = np.zeros_like(m)
+# for i in range(m.shape[0]):
+#     args_mf.lnm = np.log( m[i] )
+#     lib.setup_hmf_tinker08(ctypes.byref(args_mf), 0, pktab, pktab.shape[0], pktab.shape[1])
+#     n[i] = lib.hmf_tinker08(ctypes.byref(args_mf), 0)
+# plt.loglog(m, n, 'o-')
+# plt.show()
