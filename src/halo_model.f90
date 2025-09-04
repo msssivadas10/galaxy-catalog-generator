8ca1460daa2c63c89da3823f72a57b84aa7b3daa
ms3 <ms3.msssivadas@gmail.com>
2025-09-04 09:56:17 +0530

    movde catalog generation to seperate module

--- src/halo_model.f90
+++ src/halo_model.f90
@@ -1,10 +1,10 @@
 module halo_model_mod
-    !! Halo model calculations. These calculations are based on a 5-parameter 
-    !! halo occupation distribution (HOD) model. Includes routines for  
-    !! calculating halo averages and generating galaxy catalogs.
+    !! A module for basic halo model calculations. These calculations are based  
+    !! on a 5-parameter halo occupation distribution (HOD) model. 
 
     use iso_fortran_env, only: stderr => error_unit
     use iso_c_binding
+    use constants_mod
     use random_mod
     use interpolate_mod
     use integrate_mod
@@ -12,104 +12,25 @@ module halo_model_mod
 
     private
     public :: lagrangian_r, central_count, satellite_count, subhalo_mass_function,  &
-              halo_concentration, setup_catalog_generation, generate_galaxies,    &
-              average_halo_density, average_galaxy_density, average_satellite_frac, &
-              average_galaxy_bias
-
-    real(c_double), parameter :: delta_sc = 1.6864701998411453_c_double
-    !! Overdensity threshold for spherical collapse in EdS universe
-    
-    real(c_double), parameter :: critical_density_const = 2.775366e+07_c_double
-    !! Constant part of the present critical density.
-    !  -- 
-    !  This value is calculated sung the relation for critical density at z=0, 
-    !  `rho_c = 3*H0^2/(8*pi*G)`, where, G=6.6743e-11 m^3/kg/s^2 and H0 has 
-    !  unit km/s/Mpc. Using 1 Mpc=3.085677e+22 m and Msun=1.9884099e+30 kg, 
-    !  this value can be calculated (I used values in `astropy.constants`
-    !  to calculate this value) 
-    
-    real(c_double), parameter :: pi = 3.141592653589793_c_double
-    !! Pi
+              halo_concentration, average_halo_density, average_galaxy_density,     &
+              average_satellite_frac, average_galaxy_bias
 
     type, public, bind(c) :: hmargs_t
         !! A struct storing various halomodel parameters
-        
-        real(c_double) :: lnm_min
-        !! Minimum halo mass (in Msun) to have at least one central galaxy 
-
-        real(c_double) :: sigma_m
-        !! Width of the central galaxy transition range. (0 for a step function)
-
-        real(c_double) :: lnm0
-        !! Minimum halo mass (in Msun) to have satellite galaxies
-        
-        real(c_double) :: lnm1
-        !! Scale factor for power law satelliete count relation (Msun)
-
-        real(c_double) :: alpha
-        !! Index for the  power law satelliete count relation
-
-        real(c_double) :: scale_shmf
-        !! Scale parameter for the subhalo mass-function
-
-        real(c_double) :: slope_shmf
-        !! Slope parameter for the subhalo mass-function
-
-        real(c_double) :: z
-        !! Redshift
-        
-        real(c_double) :: H0
-        !! Hubble parameter value
-        
-        real(c_double) :: Om0
-        !! Total matter density parameter
-
-        real(c_double) :: Delta_m
-        !! Matter overdensity w.r.to mean background density
-
-        real(c_double) :: dplus
-        !! Growth factor at this redshift
-
+        real(c_double) :: lnm_min    !! Minimum halo mass (in Msun) to have at least one central galaxy 
+        real(c_double) :: sigma_m    !! Width of the central galaxy transition range. (0 for a step function)
+        real(c_double) :: lnm0       !! Minimum halo mass (in Msun) to have satellite galaxies
+        real(c_double) :: lnm1       !! Scale factor for power law satelliete count relation (Msun)
+        real(c_double) :: alpha      !! Index for the  power law satelliete count relation
+        real(c_double) :: scale_shmf !! Scale parameter for the subhalo mass-function
+        real(c_double) :: slope_shmf !! Slope parameter for the subhalo mass-function
+        real(c_double) :: z          !! Redshift
+        real(c_double) :: H0         !! Hubble parameter value
+        real(c_double) :: Om0        !! Total matter density parameter
+        real(c_double) :: Delta_m    !! Matter overdensity w.r.to mean background density
+        real(c_double) :: dplus      !! Growth factor at this redshift
     end type
 
-    type, public, bind(c) :: cgargs_t
-        !! Struct containing various parameters for galaxy catalog
-        !! generation (execpt halo model parameters). 
-
-        real(c_double) :: lnm 
-        !! Natural log of halo mass in Msun
-
-        real(c_double) :: pos(3)
-        !! Coordinates of the halo in Mpc units
-
-        real(c_double) :: lnr
-        !! Natural log of halo radius in Mpc
-
-        real(c_double) :: s
-        !! Matter variance corresponding to this halo mass
-
-        real(c_double) :: c
-        !! Concentration parameter for the halo
-
-        integer(c_int64_t) :: n_cen
-        !! Number of central galaxies (0 or 1)
-
-        integer(c_int64_t) :: n_sat
-        !! Number of satellite galaxies
-
-        integer(c_int64_t) :: rstate
-        !! Random number generator state
-
-        real(c_double) :: boxsize(3)
-        !! Size of the bounding box containing all the halos in the 
-        !! simulation. Used for periodic wrapping galaxy position.
-
-        real(c_double) :: offset(3)
-        !! Coordinates of the bottom-lower-left corner of the bounding 
-        !! box. Used for periodic wrapping galaxy position.
-
-    end type
-    
 contains
 
     function lagrangian_r(args, lnm) result(lnr) bind(c)
@@ -245,161 +166,6 @@ contains
         res = c0 * t**(-g1) * (1._c_double + t**(1._c_double / b))**(-b*(g2 - g1))
         
     end function halo_concentration
-
-! Galaxy Catalog Generation: 
-    
-    subroutine setup_catalog_generation(params, args) bind(c)
-        !! Calculate various parameters for a galaxy catalog generation
-
-        type(hmargs_t), intent(in) :: params 
-        !! Halo model parameters
-
-        type(cgargs_t), intent(inout) :: args
-        !! Arguments for catalog generation
-
-        real(c_double) :: rho_m, rho_h, p_cen, lam_sat
-
-        rho_m = params%Om0 * ( critical_density_const * params%H0**2 ) ! Matter density at z=0 in Msun/Mpc^3 
-        rho_h = rho_m ! Halo density (TODO: chek if the halo density is rho_m * self.Delta)
-
-        ! Lagrangian radius (r) corresponding to halo mass
-        args%lnr = ( args%lnm + log(3._c_double / (4*pi) / rho_h ) ) / 3._c_double ! r in Mpc
-
-        ! Central galaxy count: this is drawn from a binomial distribution of n = 1, 
-        ! so that the value is either 1 or 0. If the model uses a step function use 
-        ! the average count as the actual count. 
-        p_cen = central_count(params, args%lnm)
-        if ( abs(params%sigma_m) < 1e-08_c_double ) then
-            args%n_cen = int(p_cen, kind=c_int64_t)
-        else
-            args%n_cen = binomial_rv(args%rstate, 1_c_int64_t, p_cen)
-        end if
-        if ( args%n_cen < 1 ) return ! No central galaxies -> no satellites also 
-
-        ! Satellite galaxy count: this drawn from a poisson distribution with the 
-        ! calculated average.
-        lam_sat    = satellite_count(params, args%lnm)
-        args%n_sat = poisson_rv(args%rstate, lam_sat)
-        
-        if ( params%scale_shmf < exp(params%lnm_min - args%lnm) ) then 
-            ! Halo mass correspond to an invalid satellite galaxy mass range: no satellites
-            args%n_sat = 0
-        end if
-
-        ! Calculating halo concentration parameter
-        args%c = halo_concentration(params, args%s)
-
-    end subroutine setup_catalog_generation
-
-    subroutine generate_galaxies(params, args, n, gdata) bind(c)
-        !! Generate galaxy positions and mass.
-
-        type(hmargs_t), intent(in) :: params 
-        !! Halo model parameters
-
-        type(cgargs_t), intent(inout) :: args
-        !! Arguments for catalog generation
-
-        integer(c_int64_t), intent(in), value :: n
-        !! Size of the position and mass array: must be same total number 
-        !! of galaxies i.e., `(args%n_cen+args%n_sat)`.
-
-        real(c_double), intent(out) :: gdata(4,n)
-        !! Galaxy positions (columns 1-3) and masses (column 4). 
-
-        integer(c_int64_t) :: i
-        real(c_double) :: m_halo, r_halo, c_halo, f, r, theta, phi, Ac, k1, k2, p
-
-        if ( args%n_cen < 1 ) return ! No galaxies in this halo
-
-        m_halo = exp(args%lnm) ! Halo mass in Msun
-        r_halo = exp(args%lnr) ! Halo radius in Mpc
-        c_halo = args%c        ! Halo concentration parameter
-
-        ! Halo has a central galaxy: the position and mass of this galaxy is same
-        ! as that of the parent halo.
-        gdata(1:3,1) = args%pos(1:3) ! in Mpc
-        gdata(4  ,1) = m_halo        ! in Msun
-
-        if ( args%n_sat < 1 ) return ! No satellite galaxies in this halo
-        do i = 1, args%n_sat
-            
-            ! Assigning random mass values to the satellite galaxies: These masses 
-            ! are drown from a bounded pareto distribution, with bounds `m_min` and  
-            ! `scale_shmf*m_halo`, and slope given by slope_shmf.
-            !  
-            ! NOTE: RVs are generated using inverse transform sampling 
-            ! (<https://en.wikipedia.org/wiki/Pareto_distribution>)
-            p  = -1._c_double / params%slope_shmf
-            k1 = exp( (params%lnm_min - args%lnm)*params%slope_shmf )
-            k2 = params%scale_shmf**params%slope_shmf
-            f  = ( ( k2 - (k2 - k1) * uniform_rv(args%rstate) ) / (k1*k2) )**p ! m_sat / m_halo
-            
-            ! Generating random values corresponding to the distance of the galaxy 
-            ! from the halo center. These RVs should follow a distribution matching 
-            ! the NFW density profile of the halo. Sampling is done using the 
-            ! inverse transformation method. 
-            Ac    = uniform_rv(args%rstate)*( log(1 + c_halo) - c_halo / (1 + c_halo) )
-            r     = (r_halo / c_halo) * nfw_c(Ac)
-            theta = acos( 2*uniform_rv(args%rstate) - 1 ) ! -pi to pi
-            phi   = 2*pi*uniform_rv(args%rstate)          !   0 to 2pi
-            
-            ! Satellite galaxy coordinates x, y, and z in Mpc
-            gdata(1,i+1) = gdata(1,1) + r*sin(theta)*cos(phi)
-            gdata(2,i+1) = gdata(2,1) + r*sin(theta)*sin(phi)
-            gdata(3,i+1) = gdata(3,1) + r*cos(theta)
-            ! Periodic wrapping of coordinates to stay within the bounding box
-            call periodic_wrap( gdata(1:3,i+1), args%offset(1:3), args%boxsize(1:3) )
-            
-            ! Satellite galaxy mass in Msun
-            gdata(4,i+1) = gdata(4,1) * f
-
-        end do
-        
-    end subroutine generate_galaxies
-
-    elemental subroutine periodic_wrap(x, offset, width)
-        !! Periodicall wrap the value to the interval [offset, offset+width] 
-        real(c_double), intent(inout) :: x
-        real(c_double), intent(in)    :: offset, width
-
-        ! If width <= 0, then no wrapping is done: this can be used for forcing 
-        ! no wrapping, if needed...
-        if ( width > 0. ) then
-            ! Using `modulo` (for mathematical modulo) instead of `mod` function
-            ! (remainder of a division)
-            x = offset + modulo(x - offset, width)
-        end if
-
-    end subroutine periodic_wrap
-
-    function nfw_c(a) result(res)
-        !! Return the value of inverse to the NFW mass function. 
-
-        real(c_double), intent(in) :: a
-        real(c_double) :: res, x, p1, p2
-
-        integer, parameter :: f8 = c_double
-
-        ! Using an approximate piecewise rational function for inverting the 
-        ! c-A(c) relation. This fit works good upto A(c) ~ 10, with less that  
-        ! 10% error. For getting the actual value, one should invert the NFW 
-        ! mass function, A(c) = log(1+c) - c/(1+c).  
-        x = log10(a)
-        if ( a < 1e-03_c_double ) then
-            res = 0.50018962_f8*x + 0.15241388_f8
-        else if ( a < 2._c_double ) then
-            p1  = 2699.40545133_f8 + 2921.07235917_f8*x - 1162.90566455_f8*x**2 
-            p2  = 3705.23701996_f8 - 3065.75405505_f8*x -   61.92662277_f8*x**2
-            res = p1 / p2
-        else
-            p1  = 47.25501938_f8 + 32.98237791_f8*x - 38.26172387_f8*x**2  
-            p2  = 66.29326139_f8 - 87.37718415_f8*x + 29.86558497_f8*x**2
-            res = p1 / p2
-        end if
-        res = 10._c_double**res
-        
-    end function nfw_c
 
 ! Halo averages:
 
